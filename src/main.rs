246989e29b98991d922cc0f71ac7bde7533d98e5
vinsburg <vinsburg@gmail.com>
2024-07-08 23:09:24 +0300

    Add game start state TODO

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use sorting_game::Game;
 
 fn main() {
-    let stack_quantity = 3;
-    let stack_size = 3;
-    let units_per_kind = 3;
-    let kinds_size = 2;
+    // TODO: init the game with random state based on seed / with set of stacks.
+    let stack_quantity = 4;
+    let stack_size = 4;
+    let units_per_kind = 4;
+    let kinds_size = 3;
     let seed = 0;
 
     let mut game = Game::new(stack_quantity, stack_size, units_per_kind, kinds_size, seed);
