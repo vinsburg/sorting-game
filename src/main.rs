1efb597be35bec6fef59a337b4caf3feb2d1ddbc
vinsburg <vinsburg@gmail.com>
2024-07-08 23:01:00 +0300

    Clear the word pillar from main

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,12 @@
 use sorting_game::Game;
 
 fn main() {
-    let pillar_quantity = 3;
-    let pillar_size = 3;
+    let stack_quantity = 3;
+    let stack_size = 3;
     let units_per_kind = 3;
     let kinds_size = 2;
     let seed = 0;
 
-    let mut game = Game::new(
-        pillar_quantity,
-        pillar_size,
-        units_per_kind,
-        kinds_size,
-        seed,
-    );
+    let mut game = Game::new(stack_quantity, stack_size, units_per_kind, kinds_size, seed);
     game.turn_loop();
 }
