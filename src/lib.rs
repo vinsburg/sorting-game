9b44558956616a5e4efa18f186606d4274a65058
vinsburg <vinsburg@gmail.com>
2024-07-21 00:22:20 +0300

    Restore game0

--- src/lib.rs
+++ src/lib.rs
@@ -247,6 +247,27 @@ impl Game {
             vec![2, 7, 1, 10, 0],
             vec![9, 5, 5, 3, 9],
             vec![7, 3, 10, 9, 0],
+            vec![0, 0, 0, 0, 0],
+            vec![6, 6, 1, 0, 0],
+            vec![5, 8, 6, 0, 0],
+            vec![8, 4, 9, 0, 0],
+            vec![10, 10, 8, 6, 1],
+            vec![2, 4, 1, 10, 0],
+            vec![4, 2, 4, 0, 0],
+        ];
+        let stacks = Game::vecs_to_stacks(vec_stacks);
+        Game::new(stacks)
+    }
+
+    pub fn game_9() -> Game {
+        let vec_stacks = vec![
+            vec![1, 2, 3, 0, 0],
+            vec![5, 5, 3, 3, 4],
+            vec![6, 7, 8, 2, 8],
+            vec![9, 7, 7, 0, 0],
+            vec![2, 7, 1, 10, 0],
+            vec![9, 5, 5, 3, 9],
+            vec![7, 3, 10, 9, 0],
             // vec![0, 0, 0, 0, 0],
             vec![6, 6, 1, 0, 0],
             vec![5, 8, 6],
